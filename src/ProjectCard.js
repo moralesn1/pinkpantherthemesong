705f4a3cfcfd71b5f4be1bff38909b94ddbe3c72
Nathaniel <nathanielmorales01@gmail.com>
2020-04-03 17:06:52 +1100

    create card and render 4 cards per row

---
+++ src/ProjectCard.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { Card, Button, Row } from "react-bootstrap";
+
+export default function ProjectCard(props) {
+  return (
+    <>
+      <Card>
+        <Card.Img style={{ width: "18rem" }} variant="top" src={props.img} />
+        <Card Body>
+          <Card.Title>{props.title}</Card.Title>
+          <Card.Text>{props.text}</Card.Text>
+          <Button variant="primary">See Project</Button>
+        </Card>
+      </Card>
+    </>
+  );
+}
--- src/ProjectsPage.js
+++ src/ProjectsPage.js
@@ -1,10 +1,40 @@
 import React from "react";
+import ProjectCard from "./ProjectCard";
+import { CardDeck, Container, Row } from "react-bootstrap";
+import projects from "./projectdata.json";
 
 export default function ProjectsPage() {
+  let content = [];
+  projects.map((item, index) => {
+    if (index % 4 == 0) {
+      content.push(
+        <Row>
+          <ProjectCard key={index} title={item.title} text={item.text} />
+        </Row>
+      );
+    } else {
+      content.push(
+        <ProjectCard key={index} title={item.title} text={item.text} />
+      );
+    }
+  });
   return (
     <>
       <div>
         <h3>Projects Page</h3>
+        <Container>
+          <CardDeck>
+            {projects.map((project, index) => {
+              return (
+                <ProjectCard
+                  key={index}
+                  title={project.title}
+                  text={project.text}
+                />
+              );
+            })}
+          </CardDeck>
+        </Container>
       </div>
     </>
   );
