75ed40d24d185b058b81770ed9748eff733c5d1f
Nathaniel <nathanielmorales01@gmail.com>
2020-04-07 20:53:42 +1000

    add text and title

--- src/data/data.js
+++ src/data/data.js
@@ -9,114 +9,36 @@
 const projectData = [
   {
     img: "Hello",
-    title: "Title1",
+    title: "Ruby Terminal Project",
+    text:
+      "This terminal application was built using Ruby to be run inside the terminal. It's a choice based game with further applications for education or job training."
+  },
+  {
+    img: "Hello",
+    title: "CSS Zen Garden",
+    text:
+      "This project was aimed at developing my css skills as well as demonstrating my minimalist approach when it comes to designs."
+  },
+  {
+    img: "Hello",
+    title: "Web Portfolio",
     text: "text"
   },
   {
     img: "Hello",
-    title: "Title2",
-    text: "text"
+    title: "Mystery Box Market Place",
+    text: "A two sided market place where users buy and sell 'Mystery Boxes'."
   },
   {
     img: "Hello",
-    title: "Title3",
-    text: "text"
-  },
-  {
-    img: "Hello",
-    title: "Title4",
-    text: "text"
-  },
-  {
-    img: "Hello",
-    title: "Title5",
-    text: "text"
+    title: "Sydney Dog Training School",
+    text:
+      "A website providing information on dog training services and information utilising direct contact from potential client to customer."
   },
   {
     img: "Hello",
     title: "Title6",
     text: "text"
-  }
-];
-
-const generalData = [
-  {
-    img: "img",
-    name: "Git"
-  },
-  {
-    img: "img",
-    name: "Github"
-  },
-  {
-    img: "img",
-    name: "PostgreSQL"
-  },
-  {
-    img: "img",
-    name: "Hasura GraphQL"
-  },
-  {
-    img: "img",
-    name: "MongoDB"
-  },
-  {
-    img: "img",
-    name: "Heroku"
-  },
-  {
-    img: "img",
-    name: "AWS"
-  }
-];
-
-const languageData = [
-  {
-    img: "img",
-    name: "Ruby"
-  },
-  {
-    img: "img",
-    name: "Javascript"
-  },
-  {
-    img: "img",
-    name: "Typescript"
-  },
-  {
-    img: "img",
-    name: "HTML5"
-  },
-  {
-    img: "img",
-    name: "CSS"
-  }
-];
-
-const librariesData = [
-  {
-    img: "img",
-    name: "Rails"
-  },
-  {
-    img: "img",
-    name: "React"
-  },
-  {
-    img: "img",
-    name: "Angular"
-  },
-  {
-    img: "img",
-    name: "Node"
-  },
-  {
-    img: "img",
-    name: "Bootstrap"
-  },
-  {
-    img: "img",
-    name: "Strappy"
   }
 ];
 
